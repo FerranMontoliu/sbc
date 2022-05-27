197792976da9780d35cc56a2677713459ce1849e
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-05-27 21:19:18 +0200

    feat: Updated graph printer to display edge labels.

--- src/utils/graph_printer.py
+++ src/utils/graph_printer.py
@@ -4,12 +4,13 @@ from model.connection import Connection
 
 
 def print_graph(connections: [Connection]) -> None:
+    print('Printing graph...')
     graphviz_str: str = 'strict digraph tree {\n' \
                         '\tnode [label="\\N", shape=box, style="rounded,filled", colorscheme=blues5, color=5, fillcolor=1, width="1.2", fontname="Lucida Sans Unicode", fontsize=12];\n' \
-                        '\tedge [colorscheme=blues5, color=5, fontsize=11, fontname="Lucida Sans Unicode"];\n'
+                        '\tedge [colorscheme=blues5, color=5, fontsize=11, fontname="Lucida Sans Unicode"];\n\n'
 
     for connection in connections:
-        graphviz_str += f'\t"{connection.from_name}" -> "{connection.to_name}"\n'
+        graphviz_str += f'\t"{connection.from_name}" -> "{connection.to_name}"\t[label="Distance: {connection.distance}\\n Duration: {connection.duration}"];\n'
 
     graphviz_str += '}'
     source: Source = Source(source=graphviz_str,
@@ -17,3 +18,4 @@ def print_graph(connections: [Connection]) -> None:
                             directory='../assets/',
                             format='png')
     source.view()
+    print('Graph printed successfully.')
