af4b35384c524ffa1ccac2a593280f766916a37a
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-05-30 22:12:09 +0200

    feat: Added argparse to manage the arguments of the program.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-import sys
-from typing import Literal, Callable
+from argparse import ArgumentParser
+from typing import Callable, Literal
 
 from algorithms.a import compute_a
 from algorithms.csp import compute_csp
@@ -11,19 +11,60 @@ from utils.graph_printer import print_graph
 
 
 def compute(cities: [City],
-            algorithm: str,
+            algorithm: Literal['a', 'csp'],
             from_city: str,
             to_city: str,
             heuristic_function: Callable[[Connection], float | int]) -> ([City], float):
-    if algorithm == 'A*':
+    print('Computing algorithm...')
+    if algorithm == 'a':
         path, acc_weight = compute_a(cities, from_city, to_city, heuristic_function)
-    elif algorithm == 'CSP':
+    elif algorithm == 'csp':
         path, acc_weight = compute_csp(cities, from_city, to_city, heuristic_function)
     else:
-        raise Exception(f"ERROR. Invalid algorithm parameter: Must be 'A*' or 'CSP', but got {algorithm} instead.")
-    print(f"The best route found is:")
-    print([c.name for c in path])
-    print(f"With a cost of {acc_weight}")
+        raise Exception(f"ERROR. Invalid algorithm parameter: Must be 'a' or 'csp', but got '{algorithm}' instead.")
+    print('Search finished.')
+
+    print(f"The best route found is: {[c.name for c in path]}.")
+    print(f"With a cost of {acc_weight}.")
+
+
+def parse_arguments() -> ArgumentParser:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('-f',
+                        '--file',
+                        dest='dataset_path',
+                        metavar='dataset_path',
+                        type=str,
+                        help='Path of the dataset.',
+                        required=True)
+    parser.add_argument('-o',
+                        '--origin',
+                        dest='origin',
+                        metavar='origin',
+                        type=str,
+                        help='Name of the origin city.',
+                        required=True)
+    parser.add_argument('-d',
+                        '--destination',
+                        dest='destination',
+                        metavar='destination',
+                        type=str,
+                        help='Name of the destination city.',
+                        required=True)
+    parser.add_argument('-a',
+                        '--algorithm',
+                        dest='algorithm',
+                        metavar='algorithm',
+                        choices=['a', 'csp'],
+                        help="Name of the algorithm. Choices: ['a', 'csp'].",
+                        required=True)
+
+    parser.add_argument('-p',
+                        '--print-graph',
+                        action='store_true',
+                        dest='print_graph',
+                        help='[OPTIONAL] Enable generation of graph in GV and PNG format.')
+    return parser
 
 
 def main():
@@ -36,23 +77,17 @@ def main():
     def hf_duration_distance(c: Connection) -> int:
         return c.duration * c.distance
 
-    args: [] = sys.argv[1:]
-    num_args: int = len(args)
+    parser = parse_arguments()
 
-    if num_args < 4:
-        raise Exception(f'ERROR. Invalid number of arguments: expected at least 4 but got {num_args} instead.')
+    args = parser.parse_args()
 
-    cities = parse_json_dataset(args[0])
+    cities = parse_json_dataset(args.dataset_path)
 
-    if num_args == 5:
-        if args[4] == '--print-graph':
-            print_graph(cities)
-        else:
-            raise Exception(
-                f"ERROR. Invalid last parameter: expected '--print-tree' or nothing, but got {args[4]} instead.")
+    if args.print_graph:
+        print_graph(cities)
 
-    check_data_validity(cities, args[1], args[2])
-    compute(cities, args[3], args[1], args[2])
+    check_data_validity(cities, args.origin, args.destination)
+    compute(cities, args.algorithm, args.origin, args.destination, hf_distance)
 
 
 if __name__ == "__main__":
